914a44199753f31b4b305e46f6ab68a10bb27a79
javakung <javakung@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-22 17:25:23 +0000

    *bugfix - wrong pathname
    git-svn-id: https://plugins.svn.wordpress.org/abdul-wp-plugin/tags/0.12@265998 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ abdul.php
@@ -0,0 +1,11 @@
+﻿<?php
+include_once( dirname(__FILE__).'/abdulapi/ABDUL.php');
+$result ="";
+$q="";
+if(isset($_REQUEST['q'])){
+	$q=$_REQUEST['q'];
+	$abdul = new ABDUL;
+	$result = $abdul->getAnswer($q);	
+	echo $result;
+}
+?>
