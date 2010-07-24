0dfed0f05483c8598d4f1d2ed43aa1f7ca5ab91f
javakung <javakung@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-24 19:29:30 +0000

    minor change
    git-svn-id: https://plugins.svn.wordpress.org/abdul-wp-plugin/tags/0.1.3.3@266940 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ abdul-widget.php
@@ -0,0 +1,154 @@
+<?php
+/*
+Plugin Name: ABDUL Widget
+Plugin URI: http://wordpress.org/extend/plugins/abdul-wp-plugin/
+Description: Chat with ABDUL
+Version: 0.1.3.3
+Author: DreamBuilder Inc.
+Author URI: http://www.conan.in.th/
+*/
+
+
+global $wp_version;
+
+if ( version_compare( $wp_version, '2.9', '<' ) ) {
+//	include_once( dirname( __FILE__ ) . '/abdul/abdul.php' );
+}
+
+
+class ABDUL_Widget extends WP_Widget {
+
+	function ABDUL_Widget() {
+		$widget_ops = array('classname' => 'widget_abdul', 'description' => __( "Chat with ABDUL") );
+		$this->WP_Widget('abdul', __('ABDUL'), $widget_ops);
+	}
+
+	function widget( $args, $instance ) {
+		
+        extract( $args );
+        echo $before_widget;
+        $title = apply_filters('widget_title', $instance['title']);
+        if( $title ) echo $before_title . $title . $after_title;
+
+
+?>
+
+<script type="text/javascript" src="<?php echo WP_PLUGIN_URL;?>/abdul-wp-plugin/yui/yahoo/yahoo-min.js"></script> 
+<script type="text/javascript" src="<?php echo WP_PLUGIN_URL;?>/abdul-wp-plugin/yui/event/event-min.js"></script> 
+<script type="text/javascript" src="<?php echo WP_PLUGIN_URL;?>/abdul-wp-plugin/yui/connection/connection-min.js"></script> 
+
+<br /><br/>
+<center>
+<form id="abdul" name="abdul" onSubmit="return false;">
+<input type="text" name="q" id="q" onKeyPress="javascript:myquery(event);" size="20">
+<input type="hidden" name="from" id="from" value="wb:widget">
+<input type="hidden" name="bot" id="bot" value="abdul">
+</form> 
+<br/>
+<table border="0" style="border:0px">
+<tr style="border:none" align="left"><td width="90%" align="left" style="border:none">
+<span id="abdulanswer" style="border:0px"></span>
+</td></tr></table>
+
+</center>
+
+
+<script>
+var handleEvent = {
+		start:function(eventType, args){
+		// do something when startEvent fires.
+		document.getElementById('abdulanswer').innerHTML = "<center><img src=<?php echo WP_PLUGIN_URL;?>/abdul-wp-plugin/images/wait.gif></center>";
+		},
+
+		complete:function(eventType, args){
+		// do something when completeEvent fires.
+			document.abdul.q.select();
+		},
+
+		success:function(eventType, args){
+		// do something when successEvent fires.
+			if(args[0].responseText !== undefined){
+				document.getElementById('abdulanswer').innerHTML = args[0].responseText;
+				document.abdul.q.select();
+			}
+		},
+
+		failure:function(eventType, args){
+		// do something when failureEvent fires.
+			alert('answering system error');
+		},
+
+		abort:function(eventType, args){
+		// do something when abortEvent fires.
+		}
+	};
+
+	var callback = {
+		customevents:{
+			onStart:handleEvent.start,
+			onComplete:handleEvent.complete,
+			onSuccess:handleEvent.success,
+			onFailure:handleEvent.failure,
+			onAbort:handleEvent.abort
+		},
+		scope:handleEvent,
+	 	argument:["foo","bar","baz"]
+	};
+
+
+	function makeRequest(){
+		var q = encodeURIComponent(document.getElementById("q").value);
+		if(q!=""){
+			var sUrl = <?php echo WP_PLUGIN_URL;?>"/abdul-wp-plugin/abdul.php";
+			var data = "q="+q;
+			var request = YAHOO.util.Connect.asyncRequest('POST', sUrl, callback,data);
+		}
+	}
+
+	function myquery(e){
+		var n = e.keyCode;
+		if(n==13){//key of Enter Key
+			makeRequest();
+			document.abdul.q.select();
+		}
+		
+	}
+</script>
+
+
+<?php
+		
+		
+		
+	}
+
+    //////////////////////////////////////////////////////
+    //Update the widget settings
+    function update( $new_instance, $old_instance )
+    {
+        $instance = $old_instance;
+        $instance['title'] = $new_instance['title'];
+        return $instance;
+    }
+
+    ////////////////////////////////////////////////////
+    //Display the widget settings on the widgets admin panel
+    function form( $instance )
+    {
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php echo 'Title:'; ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />
+        </p>
+        <?php
+    }
+
+}
+
+add_action( 'widgets_init', 'abdul_widget_init' );
+
+function abdul_widget_init() {
+	register_widget('ABDUL_Widget');
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,135 @@
+=== ABDUL Widget ===
+Contributors: Guangming Sangkeettrakarn
+Tags:  widget, abdul,information,information broker,chatter bot, WordPress.com
+Stable tag: 0.1.3.3
+Requires at least: 2.8
+Tested up to: 3.0
+
+== Description ==
+ABDUL is Thai Chatter Bot providing any information from internet. 
+He works as an information broker.
+To ask him for everything you want to know
+--------
+อับดุลเป็นระบบบริการข้อมูลเชิงสนทนา ผ่านช่องทางต่างๆ 
+ให้บริการข้อมูลมากมาย ที่ผู้ใช้งานอินเทอร์เน็ตสนใจ
+
+โดยบริการที่เปิดใช้งานปัจจุบันมีดังนี้
+
+- สืบค้นข่าว
+- แปลศัพท์ 
+- แปลข้อความ
+- รายงานสภาพอากาศ
+- รายงานสภาพจราจร
+- คิดเลข
+- ตรวจผลสลากกินแบ่งฯ
+- ราคาน้ำมัน
+- ราคาทอง
+- ดัชนีตลาดหลักทรัพย์ ( realtime )
+- อัตราแลกเปลี่ยน +  แปลงค่า
+- รอบหนัง
+- QR Encoder
+
+
+== Installation ==
+1. download the file
+2. install to your system
+3. enable this plugin
+
+== Frequently Asked Questions ==
+ตัวอย่างการใช้งาน
+
+- สืบค้นข่าว
+> ข่าว
+
+- แปลศัพท์ 
+> dict dog, ดิก ต้นไม้
+
+- แปลข้อความ
+>trans i love you
+
+- รายงานสภาพอากาศ
+> อากาศปทุมธานี
+
+- รายงานสภาพจราจร
+> จราจร พญาไท
+
+- คิดเลข
+> 123*4^2
+
+- ตรวจผลสลากกินแบ่งฯ
+>หวย, ตรวจหวย 123456
+
+- ราคาน้ำมัน
+
+- ราคาทอง
+
+- ดัชนีตลาดหลักทรัพย์ ( realtime )
+>หุ้น,
+หุ้น กสิกร,
+หุ้น PTT
+
+- อัตราแลกเปลี่ยน
+> ค่าเงิน, ค่าเงินบาท
+
+- แปลงค่าอัตราแลกเปลี่ยน
+> 1000 eur (*), 1000 usd jpy  (**)
+
+*เทียบกับเงินบาท, ** สกุลเงิน
+>THB = ไทย ( บาท ),
+RUB,
+SEK = สวีเดน,
+KES,
+BND,
+PHP = ฟิลิปปินส์ ( เปโซ ),
+NZD,
+KWD,
+IDR,
+PKR,
+LAK,
+GBP  = สหราชอาณาจักร (ปอนด์สเตอร์ลิง),
+KHR = กัมพูชา (เรียล),
+CZK,
+JPY  = ญี่ปุ่น,
+BDT,
+PLN,
+SGD  = สิงคโปร์ (ดอลลาร์),
+AUD = ออสเตรเลีย (ดอลลาร์),
+KRW = เกาหลี (วอน),
+CNY = จีน (หยวน),
+EGP,
+INR  = อินเดีย,
+VND = เวียดนาม (ด็อง),
+ZAR,
+MMK = พม่า ,
+NOK = นอร์เวย์,
+USD = สหรัฐอเมริกา (ดอลลาร์),
+MYR = มาเลเซีย (ริงกิต),
+CAD = แคนาดา,
+DKK  = เดนมาร์ก
+SAR,
+MXN,
+EUR  = ยูโร,
+HKD = ฮ่องกง (ดอลลาร์),
+CHF,
+TWD,
+AED,
+
+- รอบหนัง
+> รอบหนัง [ชื่อเรื่อง/ชื่อสถานที่]
+
+- สร้าง QR Code
+> qr  [ข้อความ]
+
+== Changelog ==
+0.1
+-simple request to abdul server for every questions
+
+0.1.3.1
+add example 
+
+
+== Upgrade Notice ==
+coming sooon
+
+== Screenshots ==
+nothing now
