5d4b08145993cd9aae5ae8c8976240528db9c08f
javakung <javakung@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-24 16:12:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/abdul-wp-plugin/trunk@266849 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/abdul.js
+++ js/abdul.js
@@ -43,7 +43,7 @@ var callback = {
 function makeRequest(){
 	var q = encodeURIComponent(document.getElementById("q").value);
 	if(q!=""){
-		var sUrl = "/wp-content/plugins/abdul-wp-plugin/abdul.php";
+		var sUrl = "/wp-content/plugins/qr-encoder/abdul.php";
 		var data = "q="+q;
 		var request = YAHOO.util.Connect.asyncRequest('POST', sUrl, callback,data);
 	}
